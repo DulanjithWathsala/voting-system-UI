aae4720fce6b5ffcef5a71f2f824837341ec78c1
M.D.W.De Silva <wdulanjith@gmail.com>
2025-01-12 20:32:39 +0530

    update elction modal added

--- src/app/components/view-elections/view-elections.component.ts
+++ src/app/components/view-elections/view-elections.component.ts
@@ -2,15 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { ElectionService } from '../../services/election.service';
 import Swal from 'sweetalert2';
 import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-view-elections',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './view-elections.component.html',
   styleUrl: './view-elections.component.css',
 })
 export class ViewElectionsComponent implements OnInit {
   elections: any[] = [];
+
+  updateElectionForm = new FormGroup({
+    title: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    startDate: new FormControl('', Validators.required),
+    endDate: new FormControl('', Validators.required),
+    status: new FormControl('', Validators.required),
+  });
 
   constructor(private electionService: ElectionService) {}
 
@@ -32,4 +46,6 @@ export class ViewElectionsComponent implements OnInit {
       },
     });
   }
+
+  onSubmit(): void {}
 }
