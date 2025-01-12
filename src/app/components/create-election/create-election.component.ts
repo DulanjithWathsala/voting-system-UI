1241c680f8d4b3098b5622ddd19f63c49f3e8073
M.D.W.De Silva <wdulanjith@gmail.com>
2025-01-12 18:45:59 +0530

    form validations added

--- src/app/components/create-election/create-election.component.ts
+++ src/app/components/create-election/create-election.component.ts
@@ -1,11 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-create-election',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './create-election.component.html',
-  styleUrl: './create-election.component.css'
+  styleUrl: './create-election.component.css',
 })
 export class CreateElectionComponent {
+  createElectionForm = new FormGroup({
+    title: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    startDate: new FormControl('', Validators.required),
+    endDate: new FormControl('', Validators.required),
+    status: new FormControl('', Validators.required),
+  });
 
+  onSubmit(): void {
+    if (this.createElectionForm.valid) {
+      console.log(this.createElectionForm.value);
+    }
+  }
 }
