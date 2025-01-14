feb484c9b3b54348294e10439b4dd7f058289606
M.D.W.De Silva <wdulanjith@gmail.com>
2025-01-14 15:00:32 +0530

    create candidate api integrated

--- src/app/components/create-candidate/create-candidate.component.ts
+++ src/app/components/create-candidate/create-candidate.component.ts
@@ -1,11 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CandidateService } from '../../services/candidate.service';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import Swal from 'sweetalert2';
+import { ElectionService } from '../../services/election.service';
 
 @Component({
   selector: 'app-create-candidate',
-  imports: [],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './create-candidate.component.html',
-  styleUrl: './create-candidate.component.css'
+  styleUrl: './create-candidate.component.css',
 })
-export class CreateCandidateComponent {
+export class CreateCandidateComponent implements OnInit {
+  currentNic: string = '';
+  currentUser: any = null;
+  elections: any[] = [];
 
+  createCandidateForm = new FormGroup({
+    fullName: new FormControl('', Validators.required),
+    position: new FormControl('', Validators.required),
+    partyId: new FormControl('', Validators.required),
+    election: new FormControl('', Validators.required),
+  });
+
+  constructor(
+    private candidateService: CandidateService,
+    private electionService: ElectionService
+  ) {}
+
+  ngOnInit(): void {
+    this.electionService.getAllElections().subscribe((data: any[]) => {
+      this.elections = data;
+    });
+  }
+
+  searchOnClick(): void {
+    this.candidateService.retrieveByNic(this.currentNic).subscribe({
+      next: (data) => {
+        this.currentUser = data;
+        Swal.fire({
+          title: 'Search successful!',
+          icon: 'success',
+        });
+      },
+      error: (error) => {
+        this.currentUser = null;
+        Swal.fire({
+          title: error.error.message,
+          icon: 'error',
+        });
+      },
+    });
+  }
+
+  onSubmit(): void {
+    if (this.createCandidateForm.valid) {
+      const { election, ...canidateDetails } = this.createCandidateForm.value;
+
+      const requestBody = {
+        nic: this.currentNic,
+        election: {
+          id: election,
+        },
+        fullName: this.createCandidateForm.value.fullName,
+        position: this.createCandidateForm.value.position,
+      };
+
+      this.candidateService.create(requestBody).subscribe({
+        next: (data) => {
+          Swal.fire({
+            title: 'Election created successfully!',
+            icon: 'success',
+          }).then(() => {
+            this.createCandidateForm.reset();
+            this.currentUser = null;
+            this.currentNic = '';
+          });
+        },
+        error: (error) => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Could not save the candidate',
+          });
+        },
+      });
+    }
+  }
 }
---
+++ src/app/services/candidate.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, retry } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CandidateService {
+  private baseUrl: string = 'http://localhost:8080/api/candidate';
+
+  constructor(private http: HttpClient) {}
+
+  create(requestBody: any): Observable<any> {
+    return this.http.post<any>(`${this.baseUrl}/create`, requestBody, {
+      headers: new HttpHeaders({
+        Authorization: `${localStorage.getItem('authToken')}`,
+      }),
+    });
+  }
+
+  retrieveByNic(nic: string): Observable<any> {
+    return this.http.get<any>(`http://localhost:8080/api/user/by-nic/${nic}`, {
+      headers: new HttpHeaders({
+        Authorization: `${localStorage.getItem('authToken')}`,
+      }),
+    });
+  }
+}
